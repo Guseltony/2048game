dacae37c22e1131ba813bb476786fe24fcbd4ce3
Akanji Anthony Guseltony <devguselt@gmail.com>
2025-11-14 16:51:00 +0100

    implementing the score and the moving right functionality

--- game.js
+++ game.js
@@ -7,6 +7,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
   let squares = [];
 
+  let score = 0
+
   // create the playing board
 
   function createBoard() {
@@ -30,8 +32,82 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log(randomNumber);
     if (squares[randomNumber].innerHTML == 0) {
       squares[randomNumber].innerHTML = 2;
+
+      // checkForGameOver function
     } else generate();
   }
+
+  function moveRight() {
+    for (let i = 0; i < 16; i++) {
+      if (i % 4 === 0) {
+        console.log(i);
+        let totalOne = squares[i].innerHTML;
+        let totalTwo = squares[i + 1].innerHTML;
+        let totalThree = squares[i + 2].innerHTML;
+        let totalFour = squares[i + 3].innerHTML;
+        let row = [
+          parseInt(totalOne),
+          parseInt(totalTwo),
+          parseInt(totalThree),
+          parseInt(totalFour),
+        ];
+
+        console.log(row);
+
+        let filteredRow = row.filter((num) => num);
+        let missing = 4 - filteredRow.length;
+        let zeros = Array(missing).fill(0);
+        let newRow = zeros.concat(filteredRow);
+
+        squares[i].innerHTML = newRow[0];
+        squares[i + 1].innerHTML = newRow[1];
+        squares[i + 2].innerHTML = newRow[2];
+        squares[i + 3].innerHTML = newRow[3];
+      }
+    }
+  }
+
+  function combineRow() {
+    for (let i = 0; i < 15; i++) {
+      if (squares[i].innerHTML === squares[i + 1].innerHTML) {
+        let combinedTotal =
+          parseInt(squares[i].innerHTML) + parseInt(squares[i + 1].innerHTML);
+        squares[i].innerHTML = combinedTotal;
+        squares[i + 1].innerHTML = 0;
+        score += combinedTotal;
+        scoreDisplay.innerHTML = score;
+      }
+    }
+    // checkWin()
+  }
+
+  // assign function to keys
+
+  function control(e) {
+    if (e.key === "ArrowLeft") {
+      // keyLeft();
+    } else if (e.key === "ArrowRight") {
+      keyRight();
+    }
+  }
+
+  document.addEventListener("keydown", control);
+
+  // function keyLeft() {
+  //   moveLeft();
+  //   combineRow();
+  //   moveLeft();
+  //   generate();
+  // }
+
+  function keyRight() {
+    moveRight();
+    combineRow();
+    moveRight();
+    generate();
+  }
+
+  moveRight();
 
 
 });
